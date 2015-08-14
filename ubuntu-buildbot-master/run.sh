71bfaa8021d74bce8242b3c4baa96ef713debe4b
samuelololol <samuelololol@gmail.com>
2015-08-14 14:10:35 +0800

    update ubuntu-buildbot-master Dockerfile

--- ubuntu-buildbot-master/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-buildbot create-master -r /app
-wget -O /app/master.cfg https://raw.githubusercontent.com/samuelololol/mydockerfiles/master/ubuntu-buildbot-master/master.cfg
-buildbot start --nodaemon /app
