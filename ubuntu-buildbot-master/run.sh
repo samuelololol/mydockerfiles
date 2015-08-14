6ebf9bd15ba7101d101d072886b492cc9ab48327
samuelololol <samuelololol@gmail.com>
2015-08-14 13:55:22 +0800

    add bash header

--- ubuntu-buildbot-master/run.sh
+++ ubuntu-buildbot-master/run.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 buildbot create-master -r /app
 wget -O /app/master.cfg https://raw.githubusercontent.com/samuelololol/mydockerfiles/master/ubuntu-buildbot-master/master.cfg
 buildbot start --nodaemon /app
