066998e47675a276263346a9bf7c3f261d0184e2
samuelololol <samuelololol@gmail.com>
2015-08-14 13:50:35 +0800

    remove run_master, chmod 755 run.sh

--- ubuntu-buildbot-master/run_master.sh
+++
@@ -1,3 +0,0 @@
-buildbot create-master -r /app
-wget -o /app/master.cfg https://raw.githubusercontent.com/samuelololol/mydockerfiles/master/ubuntu-buildbot-master/master.cfg
-buildbot start --nodaemon /app
