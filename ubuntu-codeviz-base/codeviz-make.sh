ca86cb46260a0b04c91b89e7f97955079d6578e4
samuelololol <samuelololol@gmail.com>
2016-01-18 20:43:13 +0800

    update codeviz-make.sh

--- ubuntu-codeviz-base/codeviz-make.sh
+++ ubuntu-codeviz-base/codeviz-make.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-make CXX=/usr/local/gccgraph/bin/g++
+make CXX=/usr/local/gccgraph/bin/g++ CC=/usr/local/gccgraph/bin/gcc
 genfull 
-gengraph -t -g ./full.graph --no-extern -output-type png --output-layout LR
+gengraph -t -g ./full.graph --no-extern -output-type png --output-layout LR -f main
