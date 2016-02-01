9fec885de35f8e9694ed70932b3d92b6c05af41e
samuelololol <samuelololol@gmail.com>
2016-02-01 19:41:18 +0800

    Update kafka settings

--- ubuntu-kafka/README.md
+++ ubuntu-kafka/README.md
@@ -12,10 +12,25 @@ $ docker-compose scale zks=2
 
 ```
 $ docker-compose -f docker-compose-kafka.yml up -d
-$ docker-compose -f docker-compose-kafka.yml scale kafka=3
+$ docker-compose -f docker-compose-kafka.yml scale kafs=2
 ```
 
 ### standalone
 ```
 $ docker-commpose -f docker-compose-standalone up -d
+```
+
+### test
+```
+# enter kafka container
+$ docker exec -it ubuntukafka_kfkm_1 /bin/bash
+
+# create topic
+(root@xxxx):/# /opt/kafka-xxxx/bin/kafka-topics.sh --create --topic test --zookeeper zk1:2181 --partitions 1 --replication-factor 1
+
+# consume from the topic using the console producer
+(root@xxxx):/# /opt/kafka-xxxx/bin/kafka-console-consumer.sh --topic test --zookeeper zk1:2181
+
+# using another console to create producer
+(root@xxxx):/# /opt/kafka-xxxx/bin/kafka-console-producer.sh --topic test --broker-list localhost:9092
 ```
--- ubuntu-kafka/start-kafka.sh
+++ ubuntu-kafka/start-kafka.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+if [ "$#" -ne 2 ]; then
+    echo "Illegal number of parameters"
+    return 1
+fi
+
 if [[ -z "$KAFKA_ADVERTISED_HOST_NAME"  ]]; then
     export KAFKA_ADVERTISED_HOST_NAME=$(route -n | awk '/UG[ \t ]/{print $2}')
 fi
 if [[ -z "$KAFKA_BROKER_ID"  ]]; then
     export KAFKA_BROKER_ID=$(docker inspect `hostname` | jq --raw-output '.[0] | .Name' | awk -F_ '{print $3}')
-#    export KAFKA_BROKER_ID=$KAFKA_ADVERTISED_PORT
+    [ "$1" == "slave" ] && export KAFKA_BROKER_ID=$(($KAFKA_BROKER_ID + 1))
 fi
 if [[ -z "$KAFKA_LOG_DIRS" ]]; then
     export KAFKA_LOG_DIRS="/kafka/kafka-logs-$KAFKA_BROKER_ID"
@@ -13,6 +18,12 @@ if [[ -n "$KAFKA_HEAP_OPTS" ]]; then
     sed -r -i "s/(export KAFKA_HEAP_OPTS)=\"(.*)\"/\1=\"$KAFKA_HEAP_OPTS\"/g" $KAFKA_HOME/bin/kafka-server-start.sh
     unset KAFKA_HEAP_OPTS
 fi
+
+[ "$1" == "master" ] && \
+    export KAFKA_ADVERTISED_HOST_NAME=$(docker inspect `hostname` | jq --raw-output '.[0] | .NetworkSettings.IPAddress')
+[ "$1" == "slave" ] && \
+    export KAFKA_ADVERTISED_HOST_NAME=$(docker inspect ubuntukafka_kfkm_1 | jq --raw-output '.[0] | .NetworkSettings.IPAddress')
+export KAFKA_HOST_NAME=$(docker inspect `hostname` | jq --raw-output '.[0] | .NetworkSettings.IPAddress')
 
 for VAR in `env`
 do
